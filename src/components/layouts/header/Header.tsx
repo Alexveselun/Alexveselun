3d224629b8c8faebe953e2b40e3645dc2e7af1cb
alexveselun <alexveselun@gmail.com>
2024-10-23 02:31:51 +0300

    feat(header): re-worked header

--- src/components/layouts/header/Header.tsx
+++ src/components/layouts/header/Header.tsx
@@ -73,36 +73,47 @@ const Header: React.FC = () => {
     <>
       <SeoHeader />
       <header className={`header ${isScrolled ? "scrolled" : ""}`} ref={menuRef}>
-        <div className="logo">
-          <NavLink to={link} className="logo-name-div">
-            <span className="logo-name">{greeting.logo_name}</span>
-          </NavLink>
-        </div>
-        <div className={`header-menu ${menuOpen ? "open" : ""}`}>
+        <div className={`header-menu-div ${menuOpen ? "open" : ""}`}>
           <ul className={`menu ${menuOpen ? "open" : ""}`}>
-            <input
+            {/* header-menu-button */}
+            <div className="header-menu-button-div">
+              <input
               type="checkbox"
-              className="menu-btn"
-              id="menu-btn"
+              className="header-menu-btn"
+              id="header-menu-btn"
               checked={menuOpen}
               onChange={toggleMenu}
-            />
-            <label className="menu-icon" htmlFor="menu-btn">
-              <span className="navicon"></span>
-            </label>
-            <MyLink name="Home" link="/home" />
-            {pageEnabled.experience && (
+              />
+              <label className="header-menu-btn-icon" htmlFor="header-menu-btn">
+                <span className="navicon"></span>
+              </label>
+            </div>
+
+            {/* header-logo */}
+            <div className="logo-div">
+              <div className="logo">
+                <NavLink to={link}>
+                  <span className="logo-name">{greeting.logo_name}</span>
+                </NavLink>
+              </div>
+            </div>
+
+            {/* header-items */}
+            <div className="header-items-div">
+              <MyLink name="Home" link="/home" />
+              {pageEnabled.experience && (
               <MyLink name="Experience" link="/experience" />
-            )}
-            {pageEnabled.blog && <MyLink name="Gallery" link="/gallery" />}
-            {pageEnabled.projects && (
+              )}
+              {pageEnabled.blog && <MyLink name="Gallery" link="/gallery" />}
+              {pageEnabled.projects && (
               <MyLink name="Projects" link="/projects" />
-            )}
-            {pageEnabled.opensource && <MyLink name="IT" link="/opensource" />}
-            {pageEnabled.hobbies && <MyLink name="Hobbies" link="/hobbies" />}
-            {pageEnabled.newhome && <MyLink name="NewHome" link="/newhome" />}
-            <MyLink name="Contact" link="/contact" />
-            <ToggleSwitch />
+              )}
+              {pageEnabled.opensource && <MyLink name="IT" link="/opensource" />}
+              {pageEnabled.hobbies && <MyLink name="Hobbies" link="/hobbies" />}
+              {pageEnabled.newhome && <MyLink name="NewHome" link="/newhome" />}
+              <MyLink name="Contact" link="/contact" />
+              <ToggleSwitch />
+            </div>
           </ul>
         </div>
       </header>
